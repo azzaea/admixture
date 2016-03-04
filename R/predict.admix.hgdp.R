14b85fba13b36cf1fa63ddc659b7ca998af3b52d
Peter Carbonetto <pcarbonetto@ancestry.com>
2016-03-04 13:19:45 -0700

    Removed choice of penalty parameter (a) from predict.admix.hgdp.R.

--- R/predict.admix.hgdp.R
+++ R/predict.admix.hgdp.R
@@ -58,7 +58,6 @@ dyn.load("admixture.so")})
 # -----------------
 K        <- 7      # Number of ancestral populations.
 e        <- 0.001  # Probability of a genotype error.
-a        <- 1e-5   # Strength of L0-penalty.
 seed     <- 1      # Specifies the sequence of pseudorandom numbers.
 mc.cores <- 20     # Number of CPUs to use.
 
@@ -76,21 +75,14 @@ set.seed(seed)
 
 # COMPUTE MAXIMUM LIKELIHOOD ADMIXTURE ESTIMATES USING TURBOEM
 # ------------------------------------------------------------
-# For a random initialization of the admixture proportions instead of
-# initializing to the estimates from ADMIXTURE, set Q = NULL.
-#
-# r <- system.time(out <- admixture.em(geno,K,e = e,tol = 0.01,
-#                                      mc.cores = mc.cores))
-#
 cat("Fitting admixture model to data.\n")
-r <- system.time(out <-
-       admixture.em(geno,K,e = e,a = a,tol = 0.01,exact.q = FALSE,T = T,
-                    mc.cores = mc.cores))
+r <- system.time(out <- admixture.em(geno,K,e = e,tol = 0.01,
+                                     mc.cores = mc.cores))
 with(out,
      cat(sprintf("Turbo-EM completed after %d iterations and %0.1f min.\n",
                  length(loglikelihood),r["elapsed"]/60)))
 
 # SAVE RESULTS TO FILE
 # --------------------
-# save(list = c("K","e","seed","out","r"),
-#      file = "hgdp.out.RData")
+save(list = c("K","e","seed","out","r"),
+     file = "hgdp.out.RData")
